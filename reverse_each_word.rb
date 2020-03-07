f9914f9c00de6870acbe7871f4ecde3ee53c16c1
Austin Redmond <aredmond85@gmail.com>
2020-03-07 18:59:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@
 def reverse_each_word (sentence)
   reversed_sentence = []
   sentence.split(" ").collect do |word|
-    word.reverse
+    reversed_sentence << word.reverse
   end
     return reversed_sentence.join()
 end
