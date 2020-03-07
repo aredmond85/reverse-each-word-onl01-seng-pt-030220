2f129c6e47265e41358ed84d27773e31b217f3c7
Austin Redmond <aredmond85@gmail.com>
2020-03-07 18:59:35 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+# Input sentence, split the sentence into individual words
+# COLLECT each word, reverse the word, then put it back into the array reversed
+# Do not reverse the sentence just each of the words
+# PUSH each reversed word into a new Array
+def reverse_each_word (sentence)
+  reversed_sentence = []
+  sentence.split(" ").collect do |word|
+    reversed_sentence << word.reverse
+  end
+    return reversed_sentence.join(" ")
+end
